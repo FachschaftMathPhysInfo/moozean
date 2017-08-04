66edda1afdbe671d76a18b6e37697e89a48e95f6
Sedrunum <mandresus@googlemail.com>
2017-08-05 00:47:13 +0200

    Toasts (#60)
    * Löschdialoge
    
    * paginated print results, bugfix beim löschen
    
    * Grammatikfehler

---
+++ frontend/app/components/delete-dependency-dialog.js
@@ -0,0 +1,66 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  elemente: Ember.computed('objekt',function(){
+    switch(this.get('objekt').get('constructor.modelName')){
+      case 'modul' :
+        return 'Modul';
+        break;
+      case 'typ' :
+        return 'Prüfungsart';
+        break;
+      case 'examinator' :
+        return 'Prüfendes';
+        break;
+      case 'subject' :
+        return 'Fach';
+        break;
+    }
+  }),
+  dieseselements: Ember.computed('objekt',function() {
+    //console.log(this.get('objekt'));
+    if(this.get('objekt')==null) return '';
+    switch (this.get('objekt').get('constructor.modelName')){
+      case 'modul' :
+        return 'dieses Moduls';
+        break;
+      case 'typ' :
+        return 'dieser Prüfungsart';
+        break;
+      case 'examinator' :
+        return 'dieses Prüfenden';
+        break;
+      case 'subject' :
+        return 'dieses Fachs';
+        break;
+    }
+  }),
+  actions: {
+    exitDialog: function(option) {
+      if (option == "ok") {
+        let zusatz='';
+        if(this.get('objekt.constructor.modelName')=='examinator' || this.get('objekt.constructor.modelName')=='modul'){
+          zusatz='s';
+        }
+        this.get('objekt.reports').forEach((report) => {
+          if (report.get(this.get('objekt.constructor.modelName') + zusatz).length==1){
+            report.destroyRecord();
+          }
+        });
+      }
+      this.sendAction('closeDialog', option);
+    },
+    multiple: function(report) {
+      let zusatz='';
+      if(this.get('objekt.constructor.modelName')=='examinator' || this.get('objekt.constructor.modelName')=='modul'){
+        zusatz='s';
+      }
+      return DS.PromiseObject.create({
+        promise: report.get(this.get('objekt.constructor.modelName') + zusatz).then((list) => {
+          return list.length > 1;
+        })
+      });
+    },
+  }
+});
--- frontend/app/models/modul.js
+++ frontend/app/models/modul.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
-const { attr } = DS;
+const { attr, hasMany } = DS;
 export default DS.Model.extend({
   name:attr('string'),
   abbreviation:attr('string'),
-  linkModulhandbuch:attr('string')
+  linkModulhandbuch:attr('string'),
+  reports:hasMany('report')
 });
