8081a16cb6c48200a45f00698d4c5c9d36f22cdf
Henrik Reinstädtler <henrixapp@hotmail.com>
2018-02-09 14:19:56 +0100

    Uploading Limit resized, animation on save

--- frontend/app/mixins/tex-uploading.js
+++ frontend/app/mixins/tex-uploading.js
@@ -2,7 +2,9 @@ import Mixin from '@ember/object/mixin';
 
 export default Mixin.create({
   saveFile: function(additionalMessage=""){
+      this.set("savingReport",true);
       this.get('model.report').save().then(()=>{
+        this.set("savingReport",false);
       alert("Erfolgreich gespeichert!"+additionalMessage);
       this.set('model.report',this.store.createRecord('report'));
       this.set('fileName',"Datei hochladen");
