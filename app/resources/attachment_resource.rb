b4898f9e28c642bec78120cb25c26bc91c46481a
Henrik Reinstädtler <henrixapp@hotmail.com>
2018-02-11 21:01:44 +0100

    Fix encoding with text

--- app/resources/attachment_resource.rb
+++ app/resources/attachment_resource.rb
@@ -2,7 +2,7 @@ class AttachmentResource < JSONAPI::Resource
   attributes :name, :attachment, :content_type, :created_at
   def attachment
     if @model.content_type.start_with? "text"
-      return @model.pdf.force_encoding("UTF-8")
+      return @model.pdf.encode(Encoding.find('UTF-8'), {invalid: :replace, undef: :replace, replace: ''})
     else
       return Base64.encode64(@model.pdf)
     end
