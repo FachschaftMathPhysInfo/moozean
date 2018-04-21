eaa4d5cfd1303273951e0290b884f731a2a747af
Henrik Reinstädtler <henrixapp@hotmail.com>
2018-04-21 21:46:51 +0200

    Partially fixes #92 by default scoping

--- app/models/examinator.rb
+++ app/models/examinator.rb
@@ -1,4 +1,5 @@
 class Examinator < ApplicationRecord
+  default_scope{order(:surname)}
   has_many :examined_bies, dependent: :destroy
   has_many :reports, :through => :examined_bies
   scope :name_like, ->(name){ where("surname ILIKE ?","%"+name[0]+"%")}
