203a37f5461622f9b024b8ab3f3fffc5ec2d99d9
Henrik Reinstädtler <henrixapp@hotmail.com>
2018-10-29 15:11:15 +0100

    moozean un page

--- config/initializers/jsonapi-ressource.rb
+++ config/initializers/jsonapi-ressource.rb
@@ -3,6 +3,6 @@ JSONAPI.configure do |config|
   config.top_level_meta_page_count_key = :page_count
   config.top_level_meta_include_record_count = true
   config.top_level_meta_record_count_key = :record_count
-  config.default_paginator = :paged
-  config.maximum_page_size = 100
+  #config.default_paginator = :paged
+  #config.maximum_page_size = 100
 end
