4cae3132189ec51bbe580abc5957731e6717db3d
Henrik Reinstädtler <henrixapp@hotmail.com>
2018-11-02 12:46:01 +0100

    refactor simple format

--- frontend/app/helpers/simple-format.js
+++ frontend/app/helpers/simple-format.js
@@ -6,11 +6,9 @@ export function simpleFormat(input/*, hash*/) {
     let str = input;
     if (str[0]) {
       str = str[0].replace(/\r\n?/, "\n");
-      if (str.length > 0) {
-        str = str.replace(/\n\n+/g, '</p><p>');
-        str = str.replace(/\n/g, '<br />');
-        str = '<p>' + str + '</p>';
-      }
+      str = str.replace(/\n\n+/g, '</p><p>');
+      str = str.replace(/\n/g, '<br />');
+      str = '<p>' + str + '</p>';
       return htmlSafe(str);
     }
   }
