b387d3e34e92f517c74d8becea3caf964f6622af
Thorsten Buss <t.buss@stud.uni-heidelberg.de>
2019-01-14 19:14:23 +0100

    print folder sorted

--- app/resources/printoutfolder_resource.rb
+++ app/resources/printoutfolder_resource.rb
@@ -3,9 +3,11 @@ class PrintoutfolderResource < JSONAPI::Resource
   has_one :folderseries
   after_save :print_documents
   def print_documents
-    @model.folderseries.reports.each do |report|
+    @model.folderseries.reports.sort_by{ |r| 
+        [r.moduls.empty? ? 'zzz' : r.moduls.first.name, r.examination_at]
+      }.each do |report|
       report.examinators.each { |examinator|
-      report.print_document(@model.folderseries,examinator,@model.times)
+        report.print_document(@model.folderseries,examinator,@model.times)
       }
     end
   end
