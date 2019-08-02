a7b28b5f8fafc56d6b3b8a237ffd8991fd398685
Henrik Reinstädtler <reinstaedtler@stud.uni-heidelberg.de>
2019-08-02 14:16:38 +0200

    Removing ember-cli-rails

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,4 @@ Rails.application.routes.draw do
   jsonapi_resources :inmails
   jsonapi_resources :attachments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount_ember_app :frontend, to: "/"
 end
